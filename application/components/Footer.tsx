fc2ffebd3e7d281dd5c79b35a9ae503cff71bafa
Kelly Kim <hi@kellykim.co>
2023-07-22 21:03:04 +0200

    add footer, some compnents + styling/config

---
+++ application/components/Footer.tsx
@@ -0,0 +1,54 @@
+import {
+  DiscordLogo,
+  InstagramLogo,
+  TelegramLogo,
+  TwitterLogo,
+} from "@phosphor-icons/react";
+import Link from "next/link";
+
+export function Footer() {
+  return (
+    <div className="w-full flex items-center justify-center fixed bottom-0 left-0 z-50 shadow-md bg-accent">
+      <nav className="flex items-center justify-between w-full max-w-7xl py-5 px-4 border-b-2">
+        <div className="flex items-center gap-3 transition duration-150">
+          <p>© Springer</p>
+          <p>Privacy</p>
+          <p>T&C's</p>
+        </div>
+
+        <div className="flex items-center justify-between gap-3">
+          <Link
+            href=""
+            target="_blank"
+            className="text-white hover:text-white transition-colors duration-300"
+          >
+            <TelegramLogo size={20} />
+          </Link>
+          <Link
+            href=""
+            target="_blank"
+            className="text-white hover:text-white transition-colors duration-300"
+          >
+            <DiscordLogo size={20} />
+          </Link>
+
+          <Link
+            href=""
+            target="_blank"
+            className="text-white hover:text-white transition-colors duration-300"
+          >
+            <InstagramLogo size={20} />
+          </Link>
+
+          <Link
+            href=""
+            target="_blank"
+            className="text-white hover:text-white transition-colors duration-300"
+          >
+            <TwitterLogo size={20} />
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
+}
--- application/components/button.tsx
+++ application/components/button.tsx
@@ -8,11 +8,14 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        // White background + black text; blue background on hover
+        default:
+          "bg-primary text-primary-foreground hover:bg-accent-foreground",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
           "border border-input hover:bg-accent hover:text-accent-foreground",
+
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
