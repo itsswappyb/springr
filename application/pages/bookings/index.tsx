25cd88a0b352e42c08f5997847e2e396eb671421
Swapneel Bobba <swapneel.bobba@proton.me>
2023-07-22 23:43:44 +0100

    update page routes + constants

---
+++ application/pages/bookings/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Bookings = () => {
+  return (
+    <div>
+      <h3>Bookings page</h3>
+    </div>
+  );
+};
+
+export default Bookings;
---
+++ application/pages/listings/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Listings = () => {
+  return (
+    <div>
+      <h3>Listings page</h3>
+    </div>
+  );
+};
+
+export default Listings;
